c3f8d9c3417978664eb28a869d5dc5d9fcffcd8e
Farzad Siraj <farzadsiraj@gmail.com>
2021-02-02 01:45:17 +0000

    Initial commit

---
+++ student_utils.py
@@ -0,0 +1,124 @@
+import pandas as pd
+import numpy as np
+import os
+import tensorflow as tf
+
+####### STUDENTS FILL THIS OUT ######
+#Question 3
+def reduce_dimension_ndc(df, ndc_df):
+    '''
+    df: pandas dataframe, input dataset
+    ndc_df: pandas dataframe, drug code dataset used for mapping in generic names
+    return:
+        df: pandas dataframe, output dataframe with joined generic drug name
+    '''
+    ndc_df = ndc_df.drop_duplicates(subset=['NDC_Code'])
+    df = pd.merge(df, ndc_df[['Non-proprietary Name', 'NDC_Code']],
+                  left_on='ndc_code', right_on='NDC_Code', how='left')
+    df = df.rename(columns={'Non-proprietary Name': 'generic_drug_name'}).drop(columns=['ndc_code'])
+    
+    return df
+
+#Question 4
+def select_first_encounter(df):
+    '''
+    df: pandas dataframe, dataframe with all encounters
+    return:
+        - first_encounter_df: pandas dataframe, dataframe with only the first encounter for a given patient
+    '''
+    enc_id = 'encounter_id'
+    pat_id = 'patient_nbr'
+    
+    df = df.sort_values(by=[enc_id])
+    first_encs = df.groupby(pat_id)[enc_id].head(1).values
+    first_encounter_df = df[ df[enc_id].isin(first_encs) ]
+    
+    return first_encounter_df
+
+
+#Question 6
+def patient_dataset_splitter(df, patient_key='patient_nbr'):
+    '''
+    df: pandas dataframe, input dataset that will be split
+    patient_key: string, column that is the patient id
+
+    return:
+     - train: pandas dataframe,
+     - validation: pandas dataframe,
+     - test: pandas dataframe,
+    '''
+    N = len(df)
+    p1 = int(0.6*N)
+    p2 = int(0.8*N)
+    
+    indices = list(df.index)
+    np.random.shuffle(indices)
+    
+    train = df.loc[indices[:p1]]
+    validation = df.loc[indices[p1:p2]]
+    test = df.loc[indices[p2:]]
+    
+    return train, validation, test
+
+
+#Question 7
+
+def create_tf_categorical_feature_cols(categorical_col_list,
+                              vocab_dir='./diabetes_vocab/'):
+    '''
+    categorical_col_list: list, categorical field list that will be transformed with TF feature column
+    vocab_dir: string, the path where the vocabulary text files are located
+    return:
+        output_tf_list: list of TF feature columns
+    '''
+    output_tf_list = []
+    for c in categorical_col_list:
+        vocab_file_path = os.path.join(vocab_dir,  c + "_vocab.txt")
+        '''
+        Which TF function allows you to read from a text file and create a categorical feature
+        You can use a pattern like this below...
+        tf_categorical_feature_column = tf.feature_column.......
+
+        '''
+        output_tf_list.append(tf_categorical_feature_column)
+    return output_tf_list
+
+#Question 8
+def normalize_numeric_with_zscore(col, mean, std):
+    '''
+    This function can be used in conjunction with the tf feature column for normalization
+    '''
+    return (col - mean)/std
+
+
+
+def create_tf_numeric_feature(col, MEAN, STD, default_value=0):
+    '''
+    col: string, input numerical column name
+    MEAN: the mean for the column in the training data
+    STD: the standard deviation for the column in the training data
+    default_value: the value that will be used for imputing the field
+
+    return:
+        tf_numeric_feature: tf feature column representation of the input field
+    '''
+    return tf_numeric_feature
+
+#Question 9
+def get_mean_std_from_preds(diabetes_yhat):
+    '''
+    diabetes_yhat: TF Probability prediction object
+    '''
+    m = '?'
+    s = '?'
+    return m, s
+
+# Question 10
+def get_student_binary_prediction(df, col):
+    '''
+    df: pandas dataframe prediction output dataframe
+    col: str,  probability mean prediction field
+    return:
+        student_binary_prediction: pandas dataframe converting input to flattened numpy array and binary labels
+    '''
+    return student_binary_prediction
