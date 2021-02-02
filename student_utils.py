da54d3bca03b2f16c89ac1c41996f4d87026746a
Farzad Siraj <farzadsiraj@gmail.com>
2021-02-02 21:04:23 +0000

    Improved model performance and evaluation

--- student_utils.py
+++ student_utils.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import os
 import tensorflow as tf
+import functools
 
 ####### STUDENTS FILL THIS OUT ######
 #Question 3
@@ -12,10 +13,13 @@ def reduce_dimension_ndc(df, ndc_df):
     return:
         df: pandas dataframe, output dataframe with joined generic drug name
     '''
-    ndc_df = ndc_df.drop_duplicates(subset=['NDC_Code'])
-    df = pd.merge(df, ndc_df[['Non-proprietary Name', 'NDC_Code']],
-                  left_on='ndc_code', right_on='NDC_Code', how='left')
-    df = df.rename(columns={'Non-proprietary Name': 'generic_drug_name'}).drop(columns=['ndc_code'])
+    new_df = df.copy()
+    new_df = new_df.merge(
+        ndc_df[['NDC_Code', 'Proprietary Name']], left_on='ndc_code', right_on='NDC_Code')
+    new_df['generic_drug_name'] = new_df['Proprietary Name']
+    del new_df["NDC_Code"]
+    del new_df["Proprietary Name"]
+    return new_df
     
     return df
 
@@ -74,13 +78,13 @@ def create_tf_categorical_feature_cols(categorical_col_list,
     output_tf_list = []
     for c in categorical_col_list:
         vocab_file_path = os.path.join(vocab_dir,  c + "_vocab.txt")
-        '''
-        Which TF function allows you to read from a text file and create a categorical feature
-        You can use a pattern like this below...
-        tf_categorical_feature_column = tf.feature_column.......
-
-        '''
-        output_tf_list.append(tf_categorical_feature_column)
+        tf_categorical_feature_column = tf.feature_column.categorical_column_with_vocabulary_file(
+            key=c, vocabulary_file=vocab_file_path, num_oov_buckets=1)
+        if c == 'primary_diagnosis_code':
+            cat_col = tf.feature_column.embedding_column(tf_categorical_feature_column, dimension=10)        
+        else:
+            cat_col = tf.feature_column.indicator_column(tf_categorical_feature_column)
+        output_tf_list.append(cat_col)
     return output_tf_list
 
 #Question 8
@@ -102,6 +106,9 @@ def create_tf_numeric_feature(col, MEAN, STD, default_value=0):
     return:
         tf_numeric_feature: tf feature column representation of the input field
     '''
+    normalizer = functools.partial(normalize_numeric_with_zscore, mean=MEAN, std=STD)
+    tf_numeric_feature = tf.feature_column.numeric_column(
+        key=col, default_value=default_value, normalizer_fn=normalizer, dtype=tf.float64)
     return tf_numeric_feature
 
 #Question 9
@@ -109,8 +116,8 @@ def get_mean_std_from_preds(diabetes_yhat):
     '''
     diabetes_yhat: TF Probability prediction object
     '''
-    m = '?'
-    s = '?'
+    m = diabetes_yhat.mean()
+    s = diabetes_yhat.stddev()
     return m, s
 
 # Question 10
@@ -121,4 +128,6 @@ def get_student_binary_prediction(df, col):
     return:
         student_binary_prediction: pandas dataframe converting input to flattened numpy array and binary labels
     '''
+    thresh = 5
+    student_binary_prediction = np.array(df[col].apply(lambda x: 1 if x > thresh else 0).values).astype(int)
     return student_binary_prediction
